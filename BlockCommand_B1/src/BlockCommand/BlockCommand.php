0a81480e7270c17343787fa82aaabd4b75c49d9d
Night_SN <aoadid8881@naver.com>
2019-07-31 13:41:26 +0900

    Update BlockCommand.php
    UI 변경
    소스 정리

--- BlockCommand_B1/src/BlockCommand/BlockCommand.php
+++ BlockCommand_B1/src/BlockCommand/BlockCommand.php
@@ -2,23 +2,28 @@
 
 namespace BlockCommand;
 
-// 플러그인
 use pocketmine\plugin\PluginBase;
-// 이벤트
 use pocketmine\event\Listener;
-// 이벤트 플레이어
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-// 커맨드
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-// 유틸
+use pocketmine\command\PluginCommand;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\item\Item;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
+use pocketmine\Player;
+// use function file_exists;
 use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
 // 레벨
 use pocketmine\level\particle\FloatingTextParticle;
 // math
 use pocketmine\math\Vector3;
 
+// 한글깨짐방지
 class BlockCommand extends PluginBase implements Listener {
 	public function onEnable() {
 		@mkdir ( $this->getDataFolder () );
@@ -46,106 +51,153 @@ class BlockCommand extends PluginBase implements Listener {
 			$y = $tg [1];
 			$z = $tg [2];
 			$level = $tg [3];
-			$player->getLevel ()->addParticle ( new FloatingTextParticle ( new Vector3 ( $x + 0.5, $y - 0.2, $z + 0.5, $level ), '', $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] ) );
+			$player->getLevel ()->addParticle ( new FloatingTextParticle ( new Vector3 ( $x + 0.5, $y - 0.2, $z + 0.5, $level ), '', "§l§e[ §f블럭커맨드 §e]§f" . "§f\n" . $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] . "\n§e[ §f터치시 명령어 실행 §e]" ) );
 		}
 	}
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-		$command = $command->getName ();
-		$name = $sender->getName ();
-		$tag = "§a§0[ §f블럭커맨드 §0]§f";
-		if ($command == "블럭커맨드") {
-			if (! isset ( $args [0] )) {
-				$sender->sendMessage ( $tag . " /블럭커맨드 생성 [ 이름 ] [ 명령어 ]§a§0[ §f블럭커맨드를 생성합니다. §0]§f" );
-				$sender->sendMessage ( $tag . " /블럭커맨드 제거모드 §a§0[ §f블럭커맨드 제거모드를 실행합니다. §0]§f" );
-				return true;
+	public function MainUI(Player $player) {
+		$encode = [
+			'type' => 'form',
+			'title' => '§l§e[ §f블럭커맨드 §e]§f',
+			'content' => '§e§o§l§w[ §f버튼을 눌러주세요. §e]',
+			'buttons' => [
+				[
+					'text' => '§e§o§l§w[ §f블럭커맨드 생성모드 §e]'
+				],
+				[
+					'text' => '§e§o§l§w[ §f블럭커맨드 제거모드 §e]'
+				]
+			]
+		];
+		$packet = new ModalFormRequestPacket ();
+		$packet->formId = 888;
+		$packet->formData = json_encode ( $encode );
+		$player->sendDataPacket ( $packet );
+	}
+	public function NewUI(Player $player) {
+		$encode = [
+			'type' => 'custom_form',
+			'title' => '§e§o§l§w[ §f블럭커맨드 생성 §e]',
+			'content' => [
+				[
+					'type' => 'input',
+					'text' => '§e§o§l§w[ §f커맨드의 §e이름§f을 적어주세요. §e]'
+				]
+			]
+		];
+		$packet = new ModalFormRequestPacket ();
+		$packet->formId = 898;
+		$packet->formData = json_encode ( $encode );
+		$player->sendDataPacket ( $packet );
+		return true;
+	}
+	public function RatingUI(Player $player) {
+		$encode = [
+			'type' => 'custom_form',
+			'title' => '§e§o§l§w[ §f블럭커맨드 생성 §e]',
+			'content' => [
+				[
+					'type' => 'input',
+					'text' => '§e§o§l§w[ §f커맨드의 §e명령어§f를 적어주세요. §e]'
+				]
+			]
+		];
+		$packet = new ModalFormRequestPacket ();
+		$packet->formId = 899;
+		$packet->formData = json_encode ( $encode );
+		$player->sendDataPacket ( $packet );
+		return true;
+	}
+	public function onPacket(DataPacketReceiveEvent $event) {
+		$packet = $event->getPacket ();
+		$player = $event->getPlayer ();
+		$name = $player->getName ();
+		$tag = "§l§e[ §f블럭커맨드 §e]§f";
+		if ($packet instanceof ModalFormResponsePacket) {
+			$id = $packet->formId;
+			$data = json_decode ( $packet->formData, true );
+			if ($id === 898) {
+				if ($data [0]) {
+					$this->pldb [strtolower ( $name )] ["블럭이름"] = $data [0];
+					$this->save ();
+					$this->RatingUI ( $player );
+					return true;
+				}
 			}
-			switch ($args [0]) {
-				case "제거모드" :
-					if (! $sender->isOp ()) {
-						$sender->sendMessage ( $tag . " 당신은 권한이 없습니다." );
-						return true;
-					}
-					if ($this->pldb [strtolower ( $name )] ["블럭설치"] == "준비중") {
-						$sender->sendMessage ( $tag . " 블럭커맨드 제거모드를 실행 했습니다." );
-						$sender->sendMessage ( $tag . " 블럭커맨드 제거모드를 종료시키려면 명령어를 한번더 사용하세요. " );
-						$this->pldb [strtolower ( $name )] ["블럭제거모드"] = "실행중";
-						$this->save ();
-						return true;
-					} else {
-						$sender->sendMessage ( $tag . " 블럭커맨드 제거모드를 종료 했습니다." );
-						$this->pldb [strtolower ( $name )] ["블럭제거모드"] = "준비중";
-						$this->save ();
-						return true;
-					}
-					break;
-				case "생성" :
-					if (! isset ( $args [1] )) {
-						$sender->sendMessage ( $tag . " /블럭커맨드 생성 [ 이름 ] [ 명령어 ]§a§0[ §f블럭커맨드를 생성합니다. §0]§f" );
-						$sender->sendMessage ( $tag . " /블럭커맨드 제거 §a§0[ §f블럭커맨드 제거모드를 실행합니다. §0]§f" );
-						return true;
-					}
-					switch ($args [1]) {
-						case $args [1] :
-							if (! isset ( $args [1] )) {
-								$sender->sendMessage ( $tag . " /블럭커맨드 생성 [ 이름 ] [ 명령어 ]§a§0[ §f블럭커맨드를 생성합니다. §0]§f" );
-								$sender->sendMessage ( $tag . " /블럭커맨드 제거 §a§0[ §f블럭커맨드 제거모드를 실행합니다. §0]§f" );
-								return true;
-							}
-							switch ($args [2]) {
-								case $args [2] :
-									if (! $sender->isOp ()) {
-										$sender->sendMessage ( $tag . " 당신은 권한이 없습니다." );
-										return true;
-									}
-									if ($this->pldb [strtolower ( $name )] ["블럭설치"] == "준비중") {
-										$sender->sendMessage ( $tag . " 설치할 블럭을 터치하세요." );
-										$this->pldb [strtolower ( $name )] ["블럭설치"] = "진행중";
-										$this->pldb [strtolower ( $name )] ["블럭이름"] = $args [1];
-										$this->pldb [strtolower ( $name )] ["블럭커맨드"] = $args [2];
-										$this->save ();
-										return true;
-									} else {
-										$sender->sendMessage ( $tag . " 당신은 벌써 블럭커맨드를 생성중입니다." );
-										return true;
-									}
-							}
-					}
-					break;
+			if ($id === 899) {
+				if ($data [0]) {
+					$player->sendMessage ( $tag . " 설치할 블럭을 터치하세요." );
+					$this->pldb [strtolower ( $name )] ["블럭커맨드"] = $data [0];
+					$this->pldb [strtolower ( $name )] ["블럭설치"] = "진행중";
+					$this->save ();
+				}
 			}
-			return true;
+			if ($id === 888) {
+				if ($data === 0) {
+					$this->NewUI ( $player );
+					return true;
+				} elseif ($data === 1) {
+					if ( isset ( $this->pldb [strtolower ( $name )] )) {
+						if ($this->pldb [strtolower ( $name )] ["블럭제거모드"] == "준비중") {
+							$player->sendMessage ( $tag . " 블럭커맨드 제거모드를 실행 했습니다." );
+							$player->sendMessage ( $tag . " 블럭커맨드 제거모드를 종료하려면 한번더 실행하세요." );
+							$this->pldb [strtolower ( $name )] ["블럭제거모드"] = "실행중";
+							$this->save ();
+							return true;
+						}
+						if ($this->pldb [strtolower ( $name )] ["블럭제거모드"] == "실행중") {
+							$player->sendMessage ( $tag . " 블럭커맨드 제거모드를 종료 했습니다." );
+							$player->sendMessage ( $tag . " 블럭커맨드 제거모드를 실행하려면 한번더 실행하세요." );
+							$this->pldb [strtolower ( $name )] ["블럭제거모드"] = "준비중";
+							$this->save ();
+							return true;
+						}
+					}
+				}
+			}
 		}
-	} // 블럭커맨드
+	}
 	public function Interact(PlayerInteractEvent $event) {
 		$player = $event->getPlayer ();
 		$name = $player->getName ();
-		$tag = "§a§0[ §f블럭커맨드 §0]§f";
+		$tag = "§l§e[ §f블럭커맨드 §e]§f";
 		$block = $event->getBlock ();
 		$x = $block->x;
 		$y = $block->y;
 		$z = $block->z;
 		$level = $block->getLevel ()->getFolderName ();
-		if ($this->pldb [strtolower ( $name )] ["블럭제거모드"] == "실행중") {
-			if (isset ( $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] )) {
-				unset ( $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] );
+		if ( isset ( $this->pldb [strtolower ( $name )] )) {
+			if ($this->pldb [strtolower ( $name )] ["블럭제거모드"] == "실행중") {
+				if (isset ( $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] )) {
+					$player->sendMessage ( $tag . " 해당위치의 블럭커맨드를 제거 했습니다." );
+					$player->sendMessage ( $tag . " 재접속시에 팝업이 제거됩니다." );
+					unset ( $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] );
+					$this->save ();
+					return true;
+				}
+			}
+			if ($this->pldb [strtolower ( $name )] ["블럭설치"] == "진행중") {
+				$player->sendMessage ( $tag . " 해당위치의 블럭커맨드를 생성 했습니다." );
+				$this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] = $this->pldb [strtolower ( $name )] ["블럭이름"];
+				$this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭커맨드"] = $this->pldb [strtolower ( $name )] ["블럭커맨드"];
+				$player->getLevel ()->addParticle ( new FloatingTextParticle ( new Vector3 ( $x + 0.5, $y - 0.2, $z + 0.5, $level ), '', "§l§e[ §f블럭커맨드 §e]§f" . "§f\n" . $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] . "\n§e[ §f터치시 명령어 실행 §e]" ) );
+				$this->pldb [strtolower ( $name )] ["블럭설치"] = "준비중";
+				$this->pldb [strtolower ( $name )] ["블럭이름"] = "없음";
+				$this->pldb [strtolower ( $name )] ["블럭커맨드"] = "없음";
 				$this->save ();
 				return true;
 			}
+			if (isset ( $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] )) {
+				$blockcommand = $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭커맨드"];
+				$this->getServer ()->getCommandMap ()->dispatch ( $player, $blockcommand );
+				return true;
+			}
 		}
-		if ($this->pldb [strtolower ( $name )] ["블럭설치"] == "진행중") {
-			$this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] = $this->pldb [strtolower ( $name )] ["블럭이름"];
-			$this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭커맨드"] = $this->pldb [strtolower ( $name )] ["블럭커맨드"];
-			$player->getLevel ()->addParticle ( new FloatingTextParticle ( new Vector3 ( $x + 0.5, $y - 0.2, $z + 0.5, $level ), '', $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] ) );
-			$this->pldb [strtolower ( $name )] ["블럭설치"] = "준비중";
-			$this->pldb [strtolower ( $name )] ["블럭이름"] = "없음";
-			$this->pldb [strtolower ( $name )] ["블럭커맨드"] = "없음";
-			$this->save ();
-			return true;
+	}
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+		if ($cmd->getName () === '블럭커맨드') {
+			$this->MainUI ( $sender );
 		}
-		if (isset ( $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] )) {
-			$blockcommand = $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭커맨드"];
-			$this->getServer ()->getCommandMap ()->dispatch ( $player, $blockcommand );
-			return true;
-		}
+		return true;
 	}
 	public function onDisable() {
 		$this->save ();
