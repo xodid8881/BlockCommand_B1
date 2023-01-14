7f78a37cba2d455d0cf484baaa0fcd4f04784e3f
Hwa_Baeg_ <aoadid8881@naver.com>
2023-01-14 14:48:03 +0900

    Update BlockCommand.php

--- BlockCommand_B1/src/BlockCommand/BlockCommand.php
+++ BlockCommand_B1/src/BlockCommand/BlockCommand.php
@@ -14,18 +14,25 @@ use pocketmine\item\Item;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
 use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
-use pocketmine\Player;
+use pocketmine\player\Player;
+use pocketmine\Server;
 // use function file_exists;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 // 레벨
-use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\world\particle\FloatingTextParticle;
 // math
 use pocketmine\math\Vector3;
 
-// 한글깨짐방지
+use pocketmine\world\Position;
+use pocketmine\world\World;
+use pocketmine\world\WorldManager;
+
+use pocketmine\permission\DefaultPermissions;
+
 class BlockCommand extends PluginBase implements Listener {
-	public function onEnable() {
+	public function onEnable():void
+	{
 		@mkdir ( $this->getDataFolder () );
 		$this->player = new Config ( $this->getDataFolder () . "players.yml", Config::YAML );
 		$this->pldb = $this->player->getAll ();
@@ -33,21 +40,23 @@ class BlockCommand extends PluginBase implements Listener {
 		$this->blockdb = $this->block->getAll ();
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
 	}
+	
 	public function BlockCommandEvent(Player $player) {
 		foreach ( $this->blockdb as $tg => $args ) {
 			$tg = explode ( ':', $tg );
 			$x = $tg [0];
 			$y = $tg [1];
 			$z = $tg [2];
-			$level = $tg [3];
-			$player->getLevel ()->addParticle ( new FloatingTextParticle ( new Vector3 ( $x + 0.5, $y - 0.2, $z + 0.5, $level ), '', "§l§e[ §f블럭커맨드 §e]§f" . "§f\n" . $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] . "\n§e[ §f터치시 명령어 실행 §e]" ), [$player] );
+			$level = $tg[3];
+			$player->getWorld ()->addParticle ( new Vector3 ( $x + 0.5, $y - 0.2, $z + 0.5, Server::getInstance()->getWorldManager()->getWorldByName($tg[3]) ), new FloatingTextParticle ('', "§l§e[ §f블럭커맨드 §e]§f" . "§f\n" . $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] ["블럭이름"] . "\n§e[ §f터치시 명령어 실행 §e]" ), [$player] );
 		}
 	}
+	
 	public function OnJoin(PlayerJoinEvent $event) {
 		$player = $event->getPlayer ();
 		$name = $player->getName ();
 		$this->BlockCommandEvent ($player);
-		if ($player->isOp ()) {
+		if ($player->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
 			if (! isset ( $this->pldb [strtolower ( $name )] )) {
 				$this->pldb [strtolower ( $name )] ["블럭설치"] = "준비중";
 				$this->pldb [strtolower ( $name )] ["블럭이름"] = "없음";
@@ -57,6 +66,7 @@ class BlockCommand extends PluginBase implements Listener {
 			}
 		}
 	}
+	
 	public function MainUI(Player $player) {
 		$encode = [
 			'type' => 'form',
@@ -74,7 +84,7 @@ class BlockCommand extends PluginBase implements Listener {
 		$packet = new ModalFormRequestPacket ();
 		$packet->formId = 888;
 		$packet->formData = json_encode ( $encode );
-		$player->sendDataPacket ( $packet );
+		$player->getNetworkSession()->sendDataPacket ( $packet );
 	}
 	public function NewUI(Player $player) {
 		$encode = [
@@ -90,7 +100,7 @@ class BlockCommand extends PluginBase implements Listener {
 		$packet = new ModalFormRequestPacket ();
 		$packet->formId = 898;
 		$packet->formData = json_encode ( $encode );
-		$player->sendDataPacket ( $packet );
+		$player->getNetworkSession()->sendDataPacket ( $packet );
 		return true;
 	}
 	public function RatingUI(Player $player) {
@@ -107,16 +117,19 @@ class BlockCommand extends PluginBase implements Listener {
 		$packet = new ModalFormRequestPacket ();
 		$packet->formId = 899;
 		$packet->formData = json_encode ( $encode );
-		$player->sendDataPacket ( $packet );
+		$player->getNetworkSession()->sendDataPacket ( $packet );
 		return true;
 	}
 	public function onPacket(DataPacketReceiveEvent $event) {
 		$packet = $event->getPacket ();
-		$player = $event->getPlayer ();
-		$name = $player->getName ();
+		$player = $event->getOrigin()->getPlayer ();
 		$tag = "§l§e[ §f블럭커맨드 §e]§f";
 		if ($packet instanceof ModalFormResponsePacket) {
+			$name = $player->getName ();
 			$id = $packet->formId;
+			if($packet->formData == null) {
+				return true;
+			}
 			$data = json_decode ( $packet->formData, true );
 			if ($id === 898) {
 				if ($data [0]) {
@@ -164,11 +177,13 @@ class BlockCommand extends PluginBase implements Listener {
 		$name = $player->getName ();
 		$tag = "§l§e[ §f블럭커맨드 §e]§f";
 		$block = $event->getBlock ();
-		$x = $block->x;
-		$y = $block->y;
-		$z = $block->z;
-		$level = $block->getLevel ()->getFolderName ();
-		if ($player->isOp ()) {
+		$blockPos = $block->getPosition();
+		$x = $blockPos->x;
+		$y = $blockPos->y;
+		$z = $blockPos->z;
+		$level = $player->getWorld ()->getFolderName ();
+		
+		if ($player->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
 			if ( isset ( $this->pldb [strtolower ( $name )] )) {
 				if ( $this->pldb [strtolower ( $name )] ["블럭제거모드"] == "실행중" ) {
 					if ( isset ( $this->blockdb [$x . ':' . $y . ':' . $z . ':' . $level] )) {
@@ -201,18 +216,20 @@ class BlockCommand extends PluginBase implements Listener {
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
 		$tag = "§l§c[ §f경고 §c]§f";
 		if ($cmd->getName () === '블럭커맨드') {
-			if ($sender->isOp ()) {
+			if ($sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
 				$this->MainUI ( $sender );
 			} else {
-			  	$sender->sendMessage ( $tag . "권한이 없어 이용하지 못합니다." );
+				$sender->sendMessage ( $tag . "권한이 없어 이용하지 못합니다." );
 			}
 		}
 		return true;
 	}
-	public function onDisable() {
+	public function onDisable():void
+	{
 		$this->save ();
 	}
-	public function save() {
+	public function save():void
+	{
 		$this->player->setAll ( $this->pldb );
 		$this->player->save ();
 		$this->block->setAll ( $this->blockdb );
